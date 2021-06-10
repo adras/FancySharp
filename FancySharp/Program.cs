2630d5592fb53b9c88d552067f468c52c229ecd6
Andreas Kurka <andreas.kurka@viipay.onmicrosoft.com>
2021-06-10 22:58:53 +0200

    Added documentation link for records

--- FancySharp/Program.cs
+++ FancySharp/Program.cs
@@ -10,7 +10,10 @@ namespace FancySharp
         {
             MaybeTest.Run();
             Console.WriteLine("-----------------------------");
+
+            // For more information about records see: https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/tutorials/records            
             RecordTest.Run();
+            
             Console.WriteLine("-----------------------------");
             RecordTestMore.Run();
         }
